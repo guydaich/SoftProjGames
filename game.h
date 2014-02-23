f1552d1384d249163fe4a822147960453b3a8a67
davidLif <davidshani12@gmail.com>
2014-02-23 17:59:19 +0200

    add function to game.h

--- game.h
+++ game.h
@@ -3,3 +3,4 @@
 #include "tic_tac_toe_bl.h"
 
 int SDL_Init(Uint32 flags);
+element_cntrl get_default_ui_tree();
--- general_game.h
+++ general_game.h
@@ -12,10 +12,10 @@ typedef struct _general_game
 
 	char*		(*get_name)();
 	int*		(*get_initial_state)();
-	linked_list (*get_state_children)(int* game_state, int player);
-	int			(*get_state_score(int* game_state,int player));
-	int*		(*get_difficulty_levels());
-	element_cntrl (*panel_function(int* game_state));
+	linked_list (*get_state_children)(int *game_state, int player);
+	int			(*get_state_score)(int* game_state,int player);
+	int*		(*get_difficulty_levels)();
+	element_cntrl (*panel_function)(int* game_state);
 
 
 } general_game;
